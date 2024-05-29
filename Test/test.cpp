359d2ae19306bd211801a5f21877a4dcb6be34c6
Mritunjay Namdeo <namdeomritunjay@gmail.com>
2024-05-29 13:25:32 +0000

    modified:   .vscode/tasks.json 	modified:   Test/test 	modified:   Test/test.cpp 	new file:   help 	new file:   imp_shortcuts.txt

--- Test/test.cpp
+++ Test/test.cpp
@@ -19,42 +19,89 @@
 [[maybe_unused]] int f {};
 
 //Asks for a input and output it to console.
-void integerInOut(){
+void integerInOut()
+{
 
     /*
     - it uses std::cin to ask for a keyboard input.
     - stores it in a x variable
     - and prints it.
     */
+    
+    //for storing keyboard input
+    int x {};                                          
+    
+    //Prompt for asking for user input
+    std::cout << "Input a Number (Integer):" << "\n";  
+    
+    //storing the keyboard input in x
+    std::cin  >> x;
 
-    int x {};  //for storing keyboard output
-    std::cout << "Input a Number (Integer):" << "\n";  //Asking for user input
-    std::cin >> x;  //storing the input in x
-    std::cout << "You have inputted: " << x << "\n";  //Printing it go console
-
+    //Printing it go console
+    std::cout << "You have inputted: " << x << "\n";   
 }
 
 //Test for different Types of initilizations.
-void initVarOut(){
-    std::cout <<"Default Initilization:" << "\n";
+void initVarOut()
+{
+    
+    std::cout <<"Default Initilization:"     << "\n";
     std::cout << a << "\n";
-    std::cout <<"Copy Initilization:" << "\n";
+    
+    std::cout <<"Copy Initilization:"        << "\n";
     std::cout << b << "\n";
-    std::cout <<"Direct Initlization:" << "\n";
+    
+    std::cout <<"Direct Initlization:"       << "\n";
     std::cout << c << "\n";
+    
     std::cout <<"Direct list Initilization:" << "\n";
     std::cout << d << "\n";
-    std::cout <<"Copy list Initization:" << "\n";
+    
+    std::cout <<"Copy list Initization:"     << "\n";
     std::cout << e << "\n";
-    std::cout <<"Value Initilization:" << "\n";
+
+    std::cout <<"Value Initilization:"       << "\n";
     std::cout << f << "\n";
+
+}
+
+//calculates the double of the input argument and returns it
+int doubleCalc(int val)
+{
+    return 2 * val;
+}
+
+//asks for an inout and return it
+int inputAsk()
+{
+    //init for storing the var input
+    int inputValue {};
+
+    //prompt for input
+    std::cin  >> inputValue;
+
+    //return the input value
+    return inputValue;
 
 }
 
 int main(){
     
-    integerInOut();
-    initVarOut();
+    // integerInOut();
+    // initVarOut();
+
+    //asking for input
+    std::cout << "Please input a Integer to double: \n";
+    
+    //storing input
+    int inputVal {inputAsk()};
+
+    //Print the Double output
+    std::cout  << "The Double value of input is:" << doubleCalc(inputVal);
+
+
     return 0;
 
 }
+
+
