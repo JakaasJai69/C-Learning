a221e83084272d135341f1f0ae25f37d671df87b
JhakaasJai69 <namdeomritunjay@gmail.com>
2024-06-06 22:15:42 +0530

    update compile.bash

--- compile.bash
+++ compile.bash
@@ -1,1 +1,22 @@
-/usr/bin/g++ -fdiagnostics-color=always -g -std=c++23 -ggdb -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -pedantic-errors -I/root/Projects/C-Learning/includes -Werror /root/Projects/C-Learning/main.cpp /root/Projects/C-Learning/test.cpp /root/Projects/C-Learning/trans_includes.cpp -o main.out
+#!/bin/bash
+
+echo "Which project do you want to compile: \n"
+echo "1. C-Learning\n"
+read projectN
+
+if [ $projectN = "1" ]
+then
+    echo "Do you want to compile main? (y or n)\n"
+    read compileMain
+
+    if [ "$compileMain" = "y" ]
+    then
+        /usr/bin/g++ -fdiagnostics-color=always -g -std=c++23 -ggdb -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -pedantic-errors -I/root/Projects/C-Learning/includes -Werror /root/Projects/C-Learning/main.cpp /root/Projects/C-Learning/test.cpp /root/Projects/C-Learning/trans_includes.cpp -o main.out
+        ./main.out
+    else
+        echo "Input the relative name or pathname (without / as start) of file in Project: \n"
+        read compileFile
+        /usr/bin/g++ -fdiagnostics-color=always -g -std=c++23 -ggdb -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -pedantic-errors -I/root/Projects/C-Learning/includes -Werror /root/Projects/C-Learning/$compileFile -o $(basename "$compileFile").out
+        ./$(basename "$compileFile").out
+    fi
+fi
