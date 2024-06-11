feb2b4ddf611e4c61643be67eadbb52eaab3cb4f
JakaasJai69 <namdeomritunjay@gmail.com>
2024-06-11 16:04:46 +0530

    update compile

--- compile.bash
+++ compile.bash
@@ -4,18 +4,22 @@ echo "Which project do you want to compile:"
 echo "1. C-Learning (enter 1 for first and so on)"
 read projectNo
 
-if [ "projectNo" == "1" ]
+if [ "$projectNo" == "1" ]
 then
-    $projectName="C-Learning"
+    projectName="C-Learning"
 else
     echo "Invalid Project No."
+    exit 1
 fi
 
 rootPath="/root/Projects/$projectName"
-filesPath="$rootPath/main.cpp $rootPath/main_Functions.cpp $rootPath/Programs/*"
+filesPath="$rootPath/main.cpp $rootPath/main_functions.cpp $rootPath/Programs/*"
+
 echo "-Weffc++ has been removed because of plog"
+
 debugOptions="-fdiagnostics-color=always -g -std=c++23 -ggdb -Wall -Wextra -Wconversion -Wsign-conversion -pedantic-errors -Werror"
 nondebugOptions="-fdiagnostics-color=always -g -std=c++23 -O2 -DNDEBUG -pedantic-errors"
+
 includeDir="-I$rootPath/includes -I$rootPath/includes/Third_Party"
 
 # Check if the first argument is 'y' or 'n'
@@ -47,7 +51,7 @@ for file in $debugFiles; do
             modified_line="${first_line:3}"  # Remove the leading "//"
 
         else
-            modified_line="$first_line"
+            break
 
         fi
 
@@ -72,9 +76,10 @@ then
         ./main.out
     elif [ "$compileMain" == "n" ] && [ "$debug" == "y" ]
     then
-        echo "Input the relative name or pathname (without / as start) of file in Project:"
-        read compileFile
-        /usr/bin/g++ $debugOptions $includeDir $rootPath/$compileFile -o $(basename "$compileFile").out
+        echo "Input the single or multiple relative filenames separated by space(without / as start) of file in Project:"
+        read compileFiles
+        firstFile=${compileFiles%% *}
+        /usr/bin/g++ $debugOptions $includeDir $rootPath/$compileFiles -o $(basename "$firstFile").out
         ./$(basename "$compileFile").out
     elif [ "$compileMain" == "y" ] && [ "$debug" == "n" ]
     then
@@ -82,9 +87,10 @@ then
         ./main.out
     elif [ "$compileMain" == "n" ] && [ "$debug" == "n" ]
     then
-        echo "Input the relative name or pathname (without / as start) of file in Project:"
-        read compileFile
-        /usr/bin/g++ $nondebugOptions $includeDir $rootPath/$compileFile -o $(basename "$compileFile").out
+        echo "Input the single or multiple relative filenames separated by space(without / as start) of file in Project:"
+        read compileFiles
+        firstFile=${compileFiles%% *}
+        /usr/bin/g++ $nondebugOptions $includeDir $rootPath/$compileFiles -o $(basename "$firstFile").out
         ./$(basename "$compileFile").out
     fi
 fi
